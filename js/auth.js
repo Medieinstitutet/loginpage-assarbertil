39e66c89f6852534796447caa87c779d936d35c7
Assar <33634310+assarbertil@users.noreply.github.com>
2021-11-30 21:19:32 +0100

    ändrade hur inloggning sparas

--- js/auth.js
+++ js/auth.js
@@ -4,19 +4,21 @@ const users = [
 ];
 
 // Initiera databas i local storage om den inte finns
-if (!localStorage.getItem("users") || !localStorage.getItem("authenticated")) {
+if (!localStorage.getItem("users") || !localStorage.getItem("user")) {
   localStorage.setItem("users", JSON.stringify(users));
-  localStorage.setItem("authenticated", false);
+  setAuthState(false);
 }
 
 // Användas för att logga in eller ut en användare
-function setAuthState(bool) {
-  console.log(bool ? "Inloggad" : "Utloggad");
-  localStorage.setItem("authenticated", JSON.stringify(bool));
+// setAuthState("<namn>") för att logga in
+// setAuthState(false) för att logga ut
+function setAuthState(user) {
+  console.log(user ? "Inloggad" : "Utloggad");
+  localStorage.setItem("user", JSON.stringify(user));
   authStateChanged();
 }
 
-// Hjälper mig hålla koden ren när många saker händer när man loggar in/ut
+// Hjälper mig hålla koden fin när många saker händer när man loggar in/ut
 function authStateChanged() {
   updateHeaderState();
   updateContentView();
