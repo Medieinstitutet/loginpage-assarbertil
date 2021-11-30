74b5505fe9a99d7871cf9fb37878a2f71f081cbc
Assar <33634310+assarbertil@users.noreply.github.com>
2021-11-30 16:03:17 +0100

    submitting saves user to local storage

--- src/main.js
+++ src/main.js
@@ -2,3 +2,17 @@ const users = [
   { username: "Janne", password: "test" },
   { username: "Assar", password: "kattmat" },
 ];
+
+const headerAuth = document.getElementById("header-auth");
+
+const authForm = document.getElementById("auth-form");
+const usernameInput = document.getElementById("form-username");
+const passwordInput = document.getElementById("form-password");
+
+authForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  localStorage.setItem("user", JSON.stringify({ username, password }));
+  console.log("done");
+});
