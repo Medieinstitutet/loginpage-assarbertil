907570daee312182be5e6cd1156f68e8c628384e
Assar <33634310+assarbertil@users.noreply.github.com>
2021-11-30 18:31:55 +0100

    dialog helper functions made

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -1,1 +1,30 @@
-console.log("dialog.js");
+//
+// Hanterar state för dialogrutan
+//
+
+// Hänvisa till alla element
+const dialog = document.getElementById("dialog");
+const dialogContent = document.getElementById("dialog-content");
+const closeButton = document.getElementById("dialog-close");
+
+// Används för att stänga dialogrutan med ESC-knappen
+const escClose = e => e.key === "Escape" && closeDialog();
+
+// Användas för att stänga dialogrutan när man klickar utanför
+const outsideClickClose = e =>
+  !dialogContent.contains(e.target) && closeDialog();
+
+const closeDialog = () => {
+  dialog.classList.add("hidden");
+  // Ta bort alla event listeners som är kopplade till dialogrutan
+  closeButton.addEventListener("click", closeDialog);
+  document.removeEventListener("keydown", escClose);
+  document.removeEventListener("click", outsideClickClose);
+};
+
+export const openDialog = () => {
+  dialog.classList.remove("hidden");
+  closeButton.addEventListener("click", closeDialog); // Stänga med stängknappen
+  document.addEventListener("keydown", escClose); // Stänga med ESC-knappen
+  setTimeout(() => document.addEventListener("click", outsideClickClose), 0); // Stänga genom att klicka utanför
+};
